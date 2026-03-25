47f75197dd431317b355c10af4af06b909fc27b7
EdricY <yu.edric@gmail.com>
2026-03-25 12:18:03 -0400

    bump version number

--- sdk_mods/BouncyLootGod/__init__.py
+++ sdk_mods/BouncyLootGod/__init__.py
@@ -29,7 +29,7 @@ import datetime
 import random
 
 
-mod_version = "0.5"
+mod_version = "0.5.1"
 if __name__ == "builtins":
     print("running from console, attempting to reload modules")
     get_pc().ConsoleCommand("rlm BouncyLootGod.*")
