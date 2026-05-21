2bc909b4f5df98d712ab5f4056aa5c1047da30a3
Krogne <kristoffer_rogne@hotmail.com>
2026-05-21 05:18:10 +0200

    replace if true to just have commented code

--- sdk_mods/BouncyLootGod/vending.py
+++ sdk_mods/BouncyLootGod/vending.py
@@ -68,40 +68,38 @@ def use_vending_machine(obj: unreal.UObject, args: unreal.WrappedStruct, ret, fu
         return
     blg.active_vend = obj
     blg.active_vend_price = obj.FixedFeaturedItemCost
-    if True:
-        for loot_configuration in obj.Loot:
-            if loot_configuration.ConfigurationName == "FeaturedItem":
-                # print("Found FeaturedItem config")
-                item_pool = unrealsdk.construct_object("ItemPoolDefinition", blg.package, "AP_Vending_Featured_Item_Pool")
-                item_pool.MinGameStageRequirement = None
-                probability = unrealsdk.make_struct("AttributeInitializationData", BaseValueConstant=1, BaseValueScaleConstant=1)
-                if Game.get_current().name == "TPS":
-                    ibd = "GD_Baroness_Items_crocus.BalanceDefs.Baroness_Head_Baron002"
-                else:
-                    ibd = "GD_Orchid_Shields.A_Item_Custom.S_BladeShield" #might want a different IBD here 
-                inv_bal_def = unrealsdk.find_object("InventoryBalanceDefinition", ibd)
-                balanced_item = unrealsdk.make_struct("BalancedInventoryData", InvBalanceDefinition=inv_bal_def, Probability=probability, bDropOnDeath=True)
-                item_pool.BalancedItems.append(balanced_item)
-                config = loot_configuration.ItemAttachments[0]
-                item_pool_backup = config.ItemPool
-                config.ItemPool = item_pool
-                obj.ResetInventory()
-                config.ItemPool = item_pool_backup
-    else:
-        if obj.FeaturedItem is None and Game.get_current().name == "TPS":
-            for loot_configuration in obj.Loot:
-                if loot_configuration.ConfigurationName == "FeaturedItem":
-                    item_pool = unrealsdk.construct_object("ItemPoolDefinition", blg.package, "AP_TPS_VENDING_FEATURED_ITEM_POOL")
-                    item_pool.MinGameStageRequirement = None
-                    probability = unrealsdk.make_struct("AttributeInitializationData", BaseValueConstant=1, BaseValueScaleConstant=1)
-                    inv_bal_def = unrealsdk.find_object("InventoryBalanceDefinition", "GD_Baroness_Items_crocus.BalanceDefs.Baroness_Head_Baron002")
-                    balanced_item = unrealsdk.make_struct("BalancedInventoryData", InvBalanceDefinition=inv_bal_def, Probability=probability, bDropOnDeath=True)
-                    item_pool.BalancedItems.append(balanced_item)
-                    config = loot_configuration.ItemAttachments[0]
-                    item_pool_backup = config.ItemPool
-                    config.ItemPool = item_pool
-                    obj.ResetInventory()
-                    config.ItemPool = item_pool_backup
+    for loot_configuration in obj.Loot:
+        if loot_configuration.ConfigurationName == "FeaturedItem":
+            # print("Found FeaturedItem config")
+            item_pool = unrealsdk.construct_object("ItemPoolDefinition", blg.package, "AP_Vending_Featured_Item_Pool")
+            item_pool.MinGameStageRequirement = None
+            probability = unrealsdk.make_struct("AttributeInitializationData", BaseValueConstant=1, BaseValueScaleConstant=1)
+            if Game.get_current().name == "TPS":
+                ibd = "GD_Baroness_Items_crocus.BalanceDefs.Baroness_Head_Baron002"
+            else:
+                ibd = "GD_Orchid_Shields.A_Item_Custom.S_BladeShield" #might want a different IBD here 
+            inv_bal_def = unrealsdk.find_object("InventoryBalanceDefinition", ibd)
+            balanced_item = unrealsdk.make_struct("BalancedInventoryData", InvBalanceDefinition=inv_bal_def, Probability=probability, bDropOnDeath=True)
+            item_pool.BalancedItems.append(balanced_item)
+            config = loot_configuration.ItemAttachments[0]
+            item_pool_backup = config.ItemPool
+            config.ItemPool = item_pool
+            obj.ResetInventory()
+            config.ItemPool = item_pool_backup
+    # if obj.FeaturedItem is None and Game.get_current().name == "TPS":
+    #     for loot_configuration in obj.Loot:
+    #         if loot_configuration.ConfigurationName == "FeaturedItem":
+    #             item_pool = unrealsdk.construct_object("ItemPoolDefinition", blg.package, "AP_TPS_VENDING_FEATURED_ITEM_POOL")
+    #             item_pool.MinGameStageRequirement = None
+    #             probability = unrealsdk.make_struct("AttributeInitializationData", BaseValueConstant=1, BaseValueScaleConstant=1)
+    #             inv_bal_def = unrealsdk.find_object("InventoryBalanceDefinition", "GD_Baroness_Items_crocus.BalanceDefs.Baroness_Head_Baron002")
+    #             balanced_item = unrealsdk.make_struct("BalancedInventoryData", InvBalanceDefinition=inv_bal_def, Probability=probability, bDropOnDeath=True)
+    #             item_pool.BalancedItems.append(balanced_item)
+    #             config = loot_configuration.ItemAttachments[0]
+    #             item_pool_backup = config.ItemPool
+    #             config.ItemPool = item_pool
+    #             obj.ResetInventory()
+    #             config.ItemPool = item_pool_backup
     if obj.FormOfCurrency == 0:
         obj.FixedFeaturedItemCost = 100
     else:
