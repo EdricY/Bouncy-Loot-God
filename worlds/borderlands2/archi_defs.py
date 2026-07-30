db15a8800e113c74c0cd0ea3c47e47dc2a7a5082
SwiftrunnerXXY <freestyleparkour@live.com>
2026-07-29 17:32:01 -0700

    Harmville false negative fix (#180)
    The "Harmville" challenge is easily completable immediately after encountering Brick in the Burrows, and does not need FFS boss fight access.

--- worlds/borderlands2/archi_defs.py
+++ worlds/borderlands2/archi_defs.py
@@ -1540,7 +1540,7 @@ loc_data_table = {
     "Challenge LilithDLC: You Started It":                                 BL2ArchiData("DahlAbandon", 30, tags=["general"]),
     "Challenge Backburner: Make Yourself At Home":                         BL2ArchiData("Backburner", 30, jump_z_req=551, tags=["reg-based"]),
     "Challenge Backburner: Fandir Fiction":                                BL2ArchiData("Backburner", 30, other_req_regions=["FFSBossFight", "LairOfInfiniteAgony"], req_items=["License: Unique Relic"], tags=["missable", "reg-based"]),
-    "Challenge Backburner: Harmville":                                     BL2ArchiData("Backburner", 30, other_req_regions=["FFSBossFight"], tags=["reg-based"]),
+    "Challenge Backburner: Harmville":                                     BL2ArchiData("Backburner", 30, other_req_regions=["Burrows"], tags=["reg-based"]),
     "Challenge LilithDLC: The Burdens of Creation":                        BL2ArchiData("Backburner", 30, other_req_regions=["DahlAbandon", "Burrows"], tags=["reg-based"]),
     "Challenge DahlAbandon: Abandoned Records":                            BL2ArchiData("DahlAbandon", 30, other_req_regions=["HeliosFallen"], tags=["reg-based"]),
     "Challenge DahlAbandon: The Path to Paradise":                         BL2ArchiData("DahlAbandon", 30, other_req_regions=["HeliosFallen"], jump_z_req=380, tags=["reg-based"]),
