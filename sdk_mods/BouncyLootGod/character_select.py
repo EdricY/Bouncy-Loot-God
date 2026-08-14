5b94688d1b7f1033eeec2ed76b3c126f11c54713
EdricY <yu.edric@gmail.com>
2026-08-14 14:09:54 -0400

    call find_object later

--- sdk_mods/BouncyLootGod/character_select.py
+++ sdk_mods/BouncyLootGod/character_select.py
@@ -8,75 +8,84 @@ from BouncyLootGod.state import get_globals, game_is_bl2
 
 # TODO: how does this work with dlc uninstalled
 raw_mission_data = [
-    {"MissionDef": unrealsdk.find_object("MissionDefinition", 'GD_Episode01.M_Ep1_Champion'), "Status": 4, "ObjectivesProgress": [1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition", 'GD_Episode02.M_Ep2_Henchman'), "Status": 4, "ObjectivesProgress": [1,1,1,1,1,1,0,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition", 'GD_Episode02.M_Ep2a_MoreGuns'), "Status": 4, "ObjectivesProgress": [0,1,1,1,1,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition", 'GD_Episode02.M_Ep2c_Henchman'), "Status": 4, "ObjectivesProgress": [1,1,0,1,1,1,0,1,1,1,1,1,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition", 'GD_Episode02.M_Ep2b_Henchman'), "Status": 4, "ObjectivesProgress": [1,1,0,0,1,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition", 'GD_Episode03.M_Ep3_CatchARide'), "Status": 4, "ObjectivesProgress": [1,1,1,1,1,1,1,3,1,20,1,1,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition", 'GD_Episode04.M_Ep4_WelcomeToSanctuary'), "Status": 4, "ObjectivesProgress": [1,1,3,1,1,0,1,1,1,1,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition", 'GD_Episode05.M_Ep5_ThePhoenix'), "Status": 4, "ObjectivesProgress": [1,1,7,1,1,1,1,1,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition", 'GD_Episode06.M_Ep6_RescueRoland'), "Status": 4, "ObjectivesProgress": [1,1,1,1,1,1,5,1,1,1,1,1,1,1,1,1,0,0,0]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition", 'GD_Episode07.M_Ep7_ATrainToCatch'), "Status": 4, "ObjectivesProgress": [1,1,1,0,3,1,1,3,1,0,3,3,3,1,1,1,1,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition", 'GD_Episode08.M_Ep8_SanctuaryTakesOff'), "Status": 4, "ObjectivesProgress": [1,1,1,1,1,1,481,1,1,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition", 'GD_Episode09.M_Ep9_GetBackToSanctuary'), "Status": 4, "ObjectivesProgress": [1,1,1,1,1,1,1,1,0,1,1,1,1,0]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition", 'GD_Episode10.M_Ep10_BirdISTheWord'), "Status": 4, "ObjectivesProgress": [1,1,1,1,0,3,1,10,1,0,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition", 'GD_Episode11.M_Ep11_LikeATonOf'), "Status": 4, "ObjectivesProgress": [1,1,1,1,1,1,3]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition", 'GD_Episode12.M_Ep12_BecomingJack'), "Status": 4, "ObjectivesProgress": [1,1,1,1,15,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition", 'GD_Episode13.M_Ep13_KillAngel'), "Status": 4, "ObjectivesProgress": [1,1,0,1,1,1,1,1,11,1,3]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition", 'GD_Episode14.M_Ep14_SearchingTheWreckage'), "Status": 4, "ObjectivesProgress": [1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition", 'GD_Episode16.M_Ep16_LockAndLoad'), "Status": 4, "ObjectivesProgress": [1,1,1,1,1,4,1,1,1,1,1,1,1,5,15]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition", 'GD_Episode15.M_Ep15_CharacterAssassination'), "Status": 4, "ObjectivesProgress": [1,1,1,3,1,33,1,3,1,1,1,1,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition", 'GD_Episode17.M_Ep17_KillJack'), "Status": 1, "ObjectivesProgress": [1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0], "ActiveObjectiveSet": unrealsdk.find_object("MissionObjectiveSetDefinition",'GD_Episode17.M_Ep17_KillJack:FindJackSet'), "SubObjectiveSets": [], "GameStage": 32, "bNeedsRewards": False, "bHeardKickoff": True},
+    {"MissionDef":  'GD_Episode01.M_Ep1_Champion', "Status": 4, "ObjectivesProgress": [1]},
+    {"MissionDef":  'GD_Episode02.M_Ep2_Henchman', "Status": 4, "ObjectivesProgress": [1,1,1,1,1,1,0,1,1]},
+    {"MissionDef":  'GD_Episode02.M_Ep2a_MoreGuns', "Status": 4, "ObjectivesProgress": [0,1,1,1,1,1,1]},
+    {"MissionDef":  'GD_Episode02.M_Ep2c_Henchman', "Status": 4, "ObjectivesProgress": [1,1,0,1,1,1,0,1,1,1,1,1,1,1]},
+    {"MissionDef":  'GD_Episode02.M_Ep2b_Henchman', "Status": 4, "ObjectivesProgress": [1,1,0,0,1,1,1]},
+    {"MissionDef":  'GD_Episode03.M_Ep3_CatchARide', "Status": 4, "ObjectivesProgress": [1,1,1,1,1,1,1,3,1,20,1,1,1,1]},
+    {"MissionDef":  'GD_Episode04.M_Ep4_WelcomeToSanctuary', "Status": 4, "ObjectivesProgress": [1,1,3,1,1,0,1,1,1,1,1,1]},
+    {"MissionDef":  'GD_Episode05.M_Ep5_ThePhoenix', "Status": 4, "ObjectivesProgress": [1,1,7,1,1,1,1,1,1,1]},
+    {"MissionDef":  'GD_Episode06.M_Ep6_RescueRoland', "Status": 4, "ObjectivesProgress": [1,1,1,1,1,1,5,1,1,1,1,1,1,1,1,1,0,0,0]},
+    {"MissionDef":  'GD_Episode07.M_Ep7_ATrainToCatch', "Status": 4, "ObjectivesProgress": [1,1,1,0,3,1,1,3,1,0,3,3,3,1,1,1,1,1,1]},
+    {"MissionDef":  'GD_Episode08.M_Ep8_SanctuaryTakesOff', "Status": 4, "ObjectivesProgress": [1,1,1,1,1,1,481,1,1,1,1]},
+    {"MissionDef":  'GD_Episode09.M_Ep9_GetBackToSanctuary', "Status": 4, "ObjectivesProgress": [1,1,1,1,1,1,1,1,0,1,1,1,1,0]},
+    {"MissionDef":  'GD_Episode10.M_Ep10_BirdISTheWord', "Status": 4, "ObjectivesProgress": [1,1,1,1,0,3,1,10,1,0,1]},
+    {"MissionDef":  'GD_Episode11.M_Ep11_LikeATonOf', "Status": 4, "ObjectivesProgress": [1,1,1,1,1,1,3]},
+    {"MissionDef":  'GD_Episode12.M_Ep12_BecomingJack', "Status": 4, "ObjectivesProgress": [1,1,1,1,15,1,1]},
+    {"MissionDef":  'GD_Episode13.M_Ep13_KillAngel', "Status": 4, "ObjectivesProgress": [1,1,0,1,1,1,1,1,11,1,3]},
+    {"MissionDef":  'GD_Episode14.M_Ep14_SearchingTheWreckage', "Status": 4, "ObjectivesProgress": [1]},
+    {"MissionDef":  'GD_Episode16.M_Ep16_LockAndLoad', "Status": 4, "ObjectivesProgress": [1,1,1,1,1,4,1,1,1,1,1,1,1,5,15]},
+    {"MissionDef":  'GD_Episode15.M_Ep15_CharacterAssassination', "Status": 4, "ObjectivesProgress": [1,1,1,3,1,33,1,3,1,1,1,1,1,1]},
+    {"MissionDef":  'GD_Episode17.M_Ep17_KillJack', "Status": 1, "ObjectivesProgress": [1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0], "ActiveObjectiveSet": 'GD_Episode17.M_Ep17_KillJack:FindJackSet', "SubObjectiveSets": [], "GameStage": 32, "bNeedsRewards": False, "bHeardKickoff": True},
 
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_Orchid_Plot.M_Orchid_PlotMission01'), "Status": 4, "ObjectivesProgress": [1,1,1,1]}, 
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_Orchid_Plot_Mission02.M_Orchid_PlotMission02'), "Status": 4, "ObjectivesProgress": [1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_Orchid_Plot_Mission03.M_Orchid_PlotMission03'), "Status": 4, "ObjectivesProgress": [1,1,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_Orchid_Plot_Mission04.M_Orchid_PlotMission04'), "Status": 4, "ObjectivesProgress": [1,1,1,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_Orchid_Plot_Mission05.M_Orchid_PlotMission05'), "Status": 4, "ObjectivesProgress": [1,1,1,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_Orchid_Plot_Mission06.M_Orchid_PlotMission06'), "Status": 4, "ObjectivesProgress": [0,15,1,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_Orchid_Plot_Mission07.M_Orchid_PlotMission07'), "Status": 4, "ObjectivesProgress": [1,15,1,1,1,1,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_Orchid_Plot_Mission08.M_Orchid_PlotMission08'), "Status": 4, "ObjectivesProgress": [1,1,1,1,1,1,1,1,1,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_Orchid_Plot_Mission09.M_Orchid_PlotMission09'), "Status": 1, "ObjectivesProgress": [1,2,1,0,0,0,0], "ActiveObjectiveSet": unrealsdk.find_object("MissionObjectiveSetDefinition",'GD_Orchid_Plot_Mission09.M_Orchid_PlotMission09:KillPirateQueenSet'), "GameStage": 20},
+    {"MissionDef": 'GD_Orchid_Plot.M_Orchid_PlotMission01', "Status": 4, "ObjectivesProgress": [1,1,1,1]}, 
+    {"MissionDef": 'GD_Orchid_Plot_Mission02.M_Orchid_PlotMission02', "Status": 4, "ObjectivesProgress": [1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]},
+    {"MissionDef": 'GD_Orchid_Plot_Mission03.M_Orchid_PlotMission03', "Status": 4, "ObjectivesProgress": [1,1,1,1]},
+    {"MissionDef": 'GD_Orchid_Plot_Mission04.M_Orchid_PlotMission04', "Status": 4, "ObjectivesProgress": [1,1,1,1,1]},
+    {"MissionDef": 'GD_Orchid_Plot_Mission05.M_Orchid_PlotMission05', "Status": 4, "ObjectivesProgress": [1,1,1,1,1]},
+    {"MissionDef": 'GD_Orchid_Plot_Mission06.M_Orchid_PlotMission06', "Status": 4, "ObjectivesProgress": [0,15,1,1,1]},
+    {"MissionDef": 'GD_Orchid_Plot_Mission07.M_Orchid_PlotMission07', "Status": 4, "ObjectivesProgress": [1,15,1,1,1,1,1,1]},
+    {"MissionDef": 'GD_Orchid_Plot_Mission08.M_Orchid_PlotMission08', "Status": 4, "ObjectivesProgress": [1,1,1,1,1,1,1,1,1,1,1]},
+    {"MissionDef": 'GD_Orchid_Plot_Mission09.M_Orchid_PlotMission09', "Status": 1, "ObjectivesProgress": [1,2,1,0,0,0,0], "ActiveObjectiveSet": 'GD_Orchid_Plot_Mission09.M_Orchid_PlotMission09:KillPirateQueenSet', "GameStage": 20},
 
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_IrisEpisode01.M_IrisEp1_HighwayToHell'), "Status": 4, "ObjectivesProgress": [1,1,1,1,1,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_IrisEpisode01.M_IrisEp1_WTTJ'), "Status": 4, "ObjectivesProgress": [1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_IrisEpisode02.M_IrisEp2_CultOfPersonality'), "Status": 4, "ObjectivesProgress": [1,1,1,1,1,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_IrisEpisode02.M_IrisEp2_FindBattle'), "Status": 4, "ObjectivesProgress": [1,1,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_IrisEpisode03_Battle.M_IrisEp3Battle_BarFight'), "Status": 4, "ObjectivesProgress": [1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_IrisEpisode03.M_IrisEp3_ChopSuey'), "Status": 4, "ObjectivesProgress": [0,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_IrisEpisode04.M_IrisEp4_AMontage'), "Status": 4, "ObjectivesProgress": [1,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_IrisEpisode04.M_IrisEp4_TrainningWithTina'), "Status": 4, "ObjectivesProgress": [1,7,7,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_IrisEpisode04_Battle.M_IrisEp4Battle_Race'), "Status": 4, "ObjectivesProgress": [1,1,1,1,0,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_IrisEpisode04.M_IrisEp4_CherryBomb'), "Status": 4, "ObjectivesProgress": [1,0,1,1,1,1,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_IrisEpisode05.M_IrisEp5_CageMatch'), "Status": 4, "ObjectivesProgress": [1,1,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_IrisEpisode05.M_HeavensDoor'), "Status": 4, "ObjectivesProgress": [1,1,1,1,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_IrisEpisode05_Battle.M_IrisEp5Battle_FlyboyGyro'), "Status": 4, "ObjectivesProgress": [1,1,5]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_IrisEpisode05.M_IrisEp5_KickStartMyHeart'), "Status": 4, "ObjectivesProgress": [1,0,0,0,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_IrisEpisode06.M_IrisEp6_LongWayToTheTop'), "Status": 1, "ObjectivesProgress": [1,0,0,0,0], "ActiveObjectiveSet": unrealsdk.find_object("MissionObjectiveSetDefinition",'GD_IrisEpisode06.M_IrisEp6_LongWayToTheTop:MissionObjectiveSetDefinition_9'), "GameStage": 20},
+    {"MissionDef": 'GD_IrisEpisode01.M_IrisEp1_HighwayToHell', "Status": 4, "ObjectivesProgress": [1,1,1,1,1,1,1]},
+    {"MissionDef": 'GD_IrisEpisode01.M_IrisEp1_WTTJ', "Status": 4, "ObjectivesProgress": [1]},
+    {"MissionDef": 'GD_IrisEpisode02.M_IrisEp2_CultOfPersonality', "Status": 4, "ObjectivesProgress": [1,1,1,1,1,1,1]},
+    {"MissionDef": 'GD_IrisEpisode02.M_IrisEp2_FindBattle', "Status": 4, "ObjectivesProgress": [1,1,1,1]},
+    {"MissionDef": 'GD_IrisEpisode03_Battle.M_IrisEp3Battle_BarFight', "Status": 4, "ObjectivesProgress": [1,1]},
+    {"MissionDef": 'GD_IrisEpisode03.M_IrisEp3_ChopSuey', "Status": 4, "ObjectivesProgress": [0,1,1]},
+    {"MissionDef": 'GD_IrisEpisode04.M_IrisEp4_AMontage', "Status": 4, "ObjectivesProgress": [1,1,1]},
+    {"MissionDef": 'GD_IrisEpisode04.M_IrisEp4_TrainningWithTina', "Status": 4, "ObjectivesProgress": [1,7,7,1]},
+    {"MissionDef": 'GD_IrisEpisode04_Battle.M_IrisEp4Battle_Race', "Status": 4, "ObjectivesProgress": [1,1,1,1,0,1]},
+    {"MissionDef": 'GD_IrisEpisode04.M_IrisEp4_CherryBomb', "Status": 4, "ObjectivesProgress": [1,0,1,1,1,1,1,1]},
+    {"MissionDef": 'GD_IrisEpisode05.M_IrisEp5_CageMatch', "Status": 4, "ObjectivesProgress": [1,1,1,1]},
+    {"MissionDef": 'GD_IrisEpisode05.M_HeavensDoor', "Status": 4, "ObjectivesProgress": [1,1,1,1,1,1]},
+    {"MissionDef": 'GD_IrisEpisode05_Battle.M_IrisEp5Battle_FlyboyGyro', "Status": 4, "ObjectivesProgress": [1,1,5]},
+    {"MissionDef": 'GD_IrisEpisode05.M_IrisEp5_KickStartMyHeart', "Status": 4, "ObjectivesProgress": [1,0,0,0,1,1]},
+    {"MissionDef": 'GD_IrisEpisode06.M_IrisEp6_LongWayToTheTop', "Status": 1, "ObjectivesProgress": [1,0,0,0,0], "ActiveObjectiveSet": 'GD_IrisEpisode06.M_IrisEp6_LongWayToTheTop:MissionObjectiveSetDefinition_9', "GameStage": 20},
 
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_Sage_Ep1.M_Sage_Mission01'), "Status": 4, "ObjectivesProgress": [1,1,1,1,1,1,1]}, 
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_Sage_Ep3.M_Sage_Mission03'), "Status": 4, "ObjectivesProgress": [1,1,1,0,1,1]}, 
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_Sage_Ep4.M_Sage_Mission04'), "Status": 4, "ObjectivesProgress": [1,1,1,1,1,1,1,1,1]}, 
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_Sage_Ep5.M_Sage_Mission05'), "Status": 1, "ObjectivesProgress": [1,0,1,1,1,1,0,0], "ActiveObjectiveSet": unrealsdk.find_object("MissionObjectiveSetDefinition",'GD_Sage_Ep5.M_Sage_Mission05:KillBossSet'), "GameStage": 35}, 
+    {"MissionDef": 'GD_Sage_Ep1.M_Sage_Mission01', "Status": 4, "ObjectivesProgress": [1,1,1,1,1,1,1]}, 
+    {"MissionDef": 'GD_Sage_Ep3.M_Sage_Mission03', "Status": 4, "ObjectivesProgress": [1,1,1,0,1,1]}, 
+    {"MissionDef": 'GD_Sage_Ep4.M_Sage_Mission04', "Status": 4, "ObjectivesProgress": [1,1,1,1,1,1,1,1,1]}, 
+    {"MissionDef": 'GD_Sage_Ep5.M_Sage_Mission05', "Status": 1, "ObjectivesProgress": [1,0,1,1,1,1,0,0], "ActiveObjectiveSet": 'GD_Sage_Ep5.M_Sage_Mission05:KillBossSet', "GameStage": 35}, 
 
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_Aster_Plot_Mission01.M_Aster_PlotMission01'), "Status": 4, "ObjectivesProgress": [1,0,0,1,1,0,0,1,0,1,1,3,1,1,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_Aster_Plot_Mission02.M_Aster_PlotMission02'), "Status": 4, "ObjectivesProgress": [1,0,0,4,0,1,1,3,0,1,3,3,1,3,1,1,1,4]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_Aster_Plot_Mission03.M_Aster_PlotMission03'), "Status": 4, "ObjectivesProgress": [1,0,10,0,1,1,1,1,1,15,1,1,0,1,1,1,0,15,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_Aster_Plot_Mission04.M_Aster_PlotMission04'), "Status": 1, "ObjectivesProgress": [1,0,0,1,0,1,0,1,3,1,0,0,0,0], "ActiveObjectiveSet": unrealsdk.find_object("MissionObjectiveSetDefinition",'GD_Aster_Plot_Mission04.M_Aster_PlotMission04:UseTeleporterSet'), "GameStage": 35}, 
+    {"MissionDef": 'GD_Aster_Plot_Mission01.M_Aster_PlotMission01', "Status": 4, "ObjectivesProgress": [1,0,0,1,1,0,0,1,0,1,1,3,1,1,1,1]},
+    {"MissionDef": 'GD_Aster_Plot_Mission02.M_Aster_PlotMission02', "Status": 4, "ObjectivesProgress": [1,0,0,4,0,1,1,3,0,1,3,3,1,3,1,1,1,4]},
+    {"MissionDef": 'GD_Aster_Plot_Mission03.M_Aster_PlotMission03', "Status": 4, "ObjectivesProgress": [1,0,10,0,1,1,1,1,1,15,1,1,0,1,1,1,0,15,1,1]},
+    {"MissionDef": 'GD_Aster_Plot_Mission04.M_Aster_PlotMission04', "Status": 1, "ObjectivesProgress": [1,0,0,1,0,1,0,1,3,1,0,0,0,0], "ActiveObjectiveSet": 'GD_Aster_Plot_Mission04.M_Aster_PlotMission04:UseTeleporterSet', "GameStage": 35}, 
 
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_Anemone_Plot_Mission010.M_Anemone_PlotMission010'), "Status": 4, "ObjectivesProgress": [1,1,1,1,1,3,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_Anemone_Plot_Mission020.M_Anemone_PlotMission020'), "Status": 4, "ObjectivesProgress": [1,1,3,8,1,4,1,0,0,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_Anemone_Plot_Mission025.M_Anemone_PlotMission025'), "Status": 4, "ObjectivesProgress": [1,4,1,0,1,1,0,3,1,1,1,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_Anemone_Plot_Mission030.M_Anemone_PlotMission030'), "Status": 4, "ObjectivesProgress": [1,1,1,1,1,1,1,1,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_Anemone_Plot_Mission040.M_Anemone_PlotMission040'), "Status": 4, "ObjectivesProgress": [1,1,1,25,1,2,2,1,2,2,1,1,1,1,1,1,1,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_Anemone_Plot_Mission050.M_Anemone_PlotMission050'), "Status": 4, "ObjectivesProgress": [1,1,1,1,1,1,1,1,1,1,1,1,1]},
-    {"MissionDef": unrealsdk.find_object("MissionDefinition",'GD_Anemone_Plot_Mission060.M_Anemone_PlotMission060'), "Status": 1, "ObjectivesProgress": [1,1,0,1,1,0,0,0,0], "ActiveObjectiveSet": unrealsdk.find_object("MissionObjectiveSetDefinition",'GD_Anemone_Plot_Mission060.M_Anemone_PlotMission060:Set_FindGaius02'), "GameStage": 38}, 
+    {"MissionDef": 'GD_Anemone_Plot_Mission010.M_Anemone_PlotMission010', "Status": 4, "ObjectivesProgress": [1,1,1,1,1,3,1,1]},
+    {"MissionDef": 'GD_Anemone_Plot_Mission020.M_Anemone_PlotMission020', "Status": 4, "ObjectivesProgress": [1,1,3,8,1,4,1,0,0,1]},
+    {"MissionDef": 'GD_Anemone_Plot_Mission025.M_Anemone_PlotMission025', "Status": 4, "ObjectivesProgress": [1,4,1,0,1,1,0,3,1,1,1,1,1]},
+    {"MissionDef": 'GD_Anemone_Plot_Mission030.M_Anemone_PlotMission030', "Status": 4, "ObjectivesProgress": [1,1,1,1,1,1,1,1,1,1]},
+    {"MissionDef": 'GD_Anemone_Plot_Mission040.M_Anemone_PlotMission040', "Status": 4, "ObjectivesProgress": [1,1,1,25,1,2,2,1,2,2,1,1,1,1,1,1,1,1,1]},
+    {"MissionDef": 'GD_Anemone_Plot_Mission050.M_Anemone_PlotMission050', "Status": 4, "ObjectivesProgress": [1,1,1,1,1,1,1,1,1,1,1,1,1]},
+    {"MissionDef": 'GD_Anemone_Plot_Mission060.M_Anemone_PlotMission060', "Status": 1, "ObjectivesProgress": [1,1,0,1,1,0,0,0,0], "ActiveObjectiveSet": 'GD_Anemone_Plot_Mission060.M_Anemone_PlotMission060:Set_FindGaius02', "GameStage": 38}, 
 ]
 
-mission_data = [
-    unrealsdk.make_struct("MissionStatusPlayerData", **d) for d in raw_mission_data
-]
+def construct_mission_data():
+    mission_data = []
+    for rd in raw_mission_data:
+        d = {**rd}
+        if "MissionDef" in rd:
+            d["MissionDef"] = unrealsdk.find_object("MissionDefinition", d["MissionDef"])
+        if "ActiveObjectiveSet" in rd:
+            d["ActiveObjectiveSet"] = unrealsdk.find_object("MissionObjectiveSetDefinition", rd["ActiveObjectiveSet"])
+        mission_data.append(unrealsdk.make_struct("MissionStatusPlayerData", **d))
+
+    return mission_data
+
 
 player_options = {
     'AP Unlocked Commando': 'GD_DefaultProfiles.Soldier.Profile_Soldier',
@@ -116,7 +125,7 @@ def on_select_character_option(obj: unreal.UObject, args: unreal.WrappedStruct, 
 
     template_save_game = unrealsdk.find_object('PlayerSaveGame', template_name)
     new_save_game = unrealsdk.construct_object('PlayerSaveGame', template_save_game, template_obj=template_save_game)
-    new_save_game.MissionPlaythroughs[0].MissionData = mission_data
+    new_save_game.MissionPlaythroughs[0].MissionData = construct_mission_data()
     get_pc().GetWillowGlobals().GetWillowSaveGameManager().SetCachedPlayerSaveGame(0, new_save_game)
     get_pc().LoadCachedSaveGame()
     get_pc().openlArg("Glacial_P")
