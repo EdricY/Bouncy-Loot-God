5efcee7c87652817bcc9e31dc0498aac14b3928e
SwiftrunnerXXY <freestyleparkour@live.com>
2026-09-03 19:39:21 -0700

    Eridium Pump Station 2 chest logic fix
    Removed unneeded jump requirement from Eridium Pump Station 2 chest.

--- worlds/borderlands2/archi_defs.py
+++ worlds/borderlands2/archi_defs.py
@@ -1951,7 +1951,7 @@ loc_data_table = {
     "Chest Fridge: Stalker Hollow":                                 BL2ArchiData("Fridge", 15, other_req_regions=["Highlands"]),
     "Chest Fridge: Three Switch Electric Gate":                     BL2ArchiData("Fridge", 15, other_req_regions=["Highlands"]),
     "Chest Fridge: Smashhead's Cave":                               BL2ArchiData("Fridge", 15, jump_z_req=554, other_req_regions=["Highlands"]),
-    "Chest AridNexusBoneyard: Eridium Pump Station 2":              BL2ArchiData("AridNexusBoneyard", 26, jump_z_req=563),
+    "Chest AridNexusBoneyard: Eridium Pump Station 2":              BL2ArchiData("AridNexusBoneyard", 26),
     "Chest AridNexusBoneyard: Eridium Pump Station 3":              BL2ArchiData("AridNexusBoneyard", 26, jump_z_req=630), # 640?
     "Chest WindshearWaste: Blindsided":                             BL2ArchiData("WindshearWaste", 1),
     "Chest ThousandCuts: Slab UHF":                                 BL2ArchiData("ThousandCuts", 20, jump_z_req=630), # maybe hard with just 630
