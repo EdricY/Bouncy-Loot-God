68602b94b7b9d67d4ffda86330d91ee2145f8001
EdricY <yu.edric@gmail.com>
2026-09-21 00:37:24 -0400

    alternate for rakkaholics anonymous, fix syntax error in generic rakk

--- worlds/borderlands2/archi_defs.py
+++ worlds/borderlands2/archi_defs.py
@@ -522,7 +522,9 @@ quest_data_table = {
     "Torture Chairs":                                       BL2ArchiData("Sanctuary", 18, is_non_gear_reward=True, req_rules=["Quest: Hidden Journals"]),
     "Doctor's Orders":                                      BL2ArchiData("WildlifeExploitationPreserve", 19, other_req_regions=["Sanctuary"]),
     "Wildlife Preservation":                                BL2ArchiData("WildlifeExploitationPreserve", 19, other_req_regions=["Sanctuary"], is_non_gear_reward=True, tags=["story"]),
-    "Rakkaholics Anonymous":                                BL2ArchiData("Dust", 19, req_rules=["Ranged Combat"], other_req_regions=["Sanctuary"], story_req_regions=["WildlifeExploitationPreserve"], associated_gear="Unique Pistol"),  # also Unique SniperRifle
+    "Rakkaholics Anonymous":                                BL2ArchiData("Dust", 19, req_rules=["Ranged Combat"], other_req_regions=["Sanctuary"], story_req_regions=["WildlifeExploitationPreserve"], associated_gear="Unique Pistol", alternates=[ # also Unique SniperRifle
+                                                                BL2ArchiData("Dust", 19, req_items=["Vehicle Fire"], other_req_regions=["Sanctuary"], story_req_regions=["WildlifeExploitationPreserve"])
+                                                            ]),
     "Poetic License":                                       BL2ArchiData("ThousandCuts", 20, other_req_regions=["Sanctuary"], story_req_regions=["WildlifeExploitationPreserve"],),
     "Animal Rights":                                        BL2ArchiData("WildlifeExploitationPreserve", 20, other_req_regions=["Sanctuary"], associated_gear="Unique SniperRifle"),
     "Creature Slaughter: Round 1":                          BL2ArchiData("NaturalSelectionAnnex", 20, is_non_gear_reward=True),
@@ -1478,7 +1480,7 @@ loc_data_table = {
 
     # Generic Mobs
     "Generic: Skag":                            BL2ArchiData("ThreeHornsValley", 8),
-    "Generic: Rakk":                            BL2ArchiData("SouthernShelf", 2, alternates[
+    "Generic: Rakk":                            BL2ArchiData("SouthernShelf", 2, alternates=[
                                                     BL2ArchiData("ScyllasGrove", 30),
     ]),
     "Generic: Bullymong":                       BL2ArchiData("WindshearWaste", 1, alternates=[
